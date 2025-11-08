6e7fafab8b9451f2d0a9e1f7f137bfdc86806af5
Arjun <23110040@iitgn.ac.in>
2025-11-08 20:15:55 +0530

    Initial commit

---
+++ lab10/ConstructorDataControl/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    private int data;
+    private static int activeObjects = 0;
+
+    public Program()
+    {
+        activeObjects++;
+        Console.WriteLine("Constructor Called. Active objects: " + activeObjects);
+    }
+
+    ~Program()
+    {
+        activeObjects--;
+        Console.WriteLine("Object Destroyed. Active objects: " + activeObjects);
+    }
+
+    public void set_data(int x)
+    {
+        data = x;
+    }
+
+    public void show_data()
+    {
+        Console.WriteLine("Data = " + data);
+    }
+
+    static void Main(string[] args)
+    {
+        Program? p1 = new Program();
+        Program? p2 = new Program();
+        Program? p3 = new Program();
+
+        p1.set_data(10);
+        p2.set_data(20);
+        p3.set_data(30);
+
+        p1.show_data();
+        p2.show_data();
+        p3.show_data();
+
+        Console.WriteLine("\n--- End of Program objects section ---");
+
+        p1 = null;
+        p2 = null;
+        p3 = null;
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        // Thread.Sleep(5000); // Give GC time
+
+        Console.WriteLine("--- Program End ---");
+    }
+}
---
+++ lab10/Inheritence/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+// Base class
+class Vehicle
+{
+    protected int speed;
+    protected int fuel;
+
+    public Vehicle(int speed, int fuel)
+    {
+        this.speed = speed;
+        this.fuel = fuel;
+    }
+
+    public virtual void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}");
+    }
+
+    public virtual void Drive()
+    {
+        fuel -= 5;
+        Console.WriteLine("Vehicle is moving...");
+    }
+}
+
+// Derived class Car
+class Car : Vehicle
+{
+    private int passengers;
+
+    public Car(int speed, int fuel, int passengers)
+        : base(speed, fuel)
+    {
+        this.passengers = passengers;
+    }
+
+    public override void Drive()
+    {
+        fuel -= 10;
+        Console.WriteLine($"Car is moving with passengers ({passengers})...");
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}, Passengers: {passengers}");
+    }
+}
+
+// Derived class Truck
+class Truck : Vehicle
+{
+    private int cargoWeight;
+
+    public Truck(int speed, int fuel, int cargoWeight)
+        : base(speed, fuel)
+    {
+        this.cargoWeight = cargoWeight;
+    }
+
+    public override void Drive()
+    {
+        fuel -= 15;
+        Console.WriteLine($"Truck is moving with cargo ({cargoWeight} kg)...");
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}, Cargo Weight: {cargoWeight} kg");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle v = new Vehicle(100, 50);
+        Car c = new Car(120, 60, 4);
+        Truck t = new Truck(80, 70, 2000);
+
+        Vehicle[] vehicles = { v, c, t };
+
+        foreach (Vehicle vehicle in vehicles)
+        {
+            vehicle.Drive();
+            vehicle.ShowInfo();
+            Console.WriteLine();
+        }
+    }
+}
